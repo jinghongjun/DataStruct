1e73fc7baf1301c2949747b30ae93e9b09493859
b690 <b690@borqs.com>
2015-03-18 10:27:03 +0800

    add tree

---
+++ src/com/datastruct/noline/Fig.java
@@ -0,0 +1,257 @@
+package com.datastruct.noline;
+
+import java.util.Scanner;
+
+/*
+图
+    图结构也是一种非线性数据结构
+    研究图结构的一个专门理论工具便是图论
+    图结构包括两部分：
+        顶点：图中的数据元素
+        边：图中连接这些顶点的线
+        所有的顶点构成一个顶点集合（V（G）），所有的边构成边集合（E（G）），一个完整的图结构就是由顶点集合和边集合组成的（G=（V，E））。
+    注意：图结构中顶点集合必须为非空，即必须包含一个顶点，而图结构中边集合可以为空。
+    
+基本概念
+
+    1.无向图
+        如果一个图结构中，所有的边都没有方向性，那么这种图便称为无向图
+        一条边可以用（v1,v5）或（v5,v1）表示
+        
+    2.有向图
+        如果一个图结构中，边是有方向性的，那么这种图便称为有向图。这样，我们在表示边的时候对两个顶点的顺序有要求，并且为了同无向边区分
+，这里我们采用尖括号表示有向边<v3,v4>
+
+    3.顶点的度
+        连接顶点的边的数量称为该顶点的度。顶点的度在有向图和无向图中具有不同的表示。
+        对于无向图，一个顶点V的度比较简单，其是连接该顶点的边的数量，记为D（V）。
+        对于有向图，根据连接顶点V的边的方向性，一个顶点的度有入度（以该顶点为端点的入边数量,ID(V)）和出度（以该顶点为端点的出边数量,OD(V)）之分。这样
+有向图中，一个顶点V的总度便是入度和出度之和，即D(V)=ID(V)+OD(V);
+
+    4.邻接顶点
+        邻接顶点是指图结构中一条边的两个顶点。邻接顶点在有向图和无向图具有不同的表示
+        对于无向图，邻接顶点比较简单。
+        对于有向图，根据连接顶点V的边的方向性，两个顶点分别称为起始顶点和结束顶点。有向图的邻接顶点分为两类：
+            入边邻接顶点：连接该顶点的边中的起始顶点。
+            出边邻接顶点：连接该顶点的边中的结束顶点。
+            
+    5.无向完全图
+        如果在一个无向图中，每两个顶点之间都存在一条边，那么这种图结构称为无向完全图。对于一个包含N的顶点的无向完全图，其总的边数N(N+1)/2
+        
+    6.有向完全图
+        如果一个有向图，每两个顶点之间都存在方向相反的两条边，那么这种图结构成为有向完全图。对于一个包含N的顶点的有向弯曲图，其总的边数N(N-1)
+    
+    7.子图
+        子图的概念类似的子集合，由于一个完整的图结构包括顶点和边，因此，一个子图的顶点和边都应该是另一个图结构的子集合。这里需要强调
+的是，只有顶点集合是子集的，或者只有边集合是子集，都不是子图。
+
+    8.路径
+        路径就是图结构中两个顶点之间的连线。
+        图结构中的路径还可以细分
+            简单路径
+            环
+            简单回路
+     
+     9.连通、连通图和连通分量
+         连通:如果图结构中两个顶点之间有路径，则称这两个顶点是连通的。这里需要注意连通的两个顶点可以不是邻接顶点，只要有路径连接即可，可以途径多个顶点
+         连通图：如果无向图中任意两个顶点都是连通的，那么这个图便称连通图。否则反之。
+         连通分量：无向图的极大连通子图称为该图的连通分量
+         
+     10.强连通图和强连通分量
+        这个概念类似无向图，这个针对有向图而言的。
+        
+     11.权
+         在实际的应用中往往需要将边表示成某种数值，这个数值便是该边的权。无向图中加入权值，则称为无向带权图，有向图加入权值，则称为有向带权图。
+         权在实际应用中可以代表各种含义，例如在交通图中表示了道路的长度，在通信网络中表示基站之间的距离，在人际关系中代表亲密程度。
+     
+     12.网
+         网就是边上带有权值的图的另一种名称。
+         
+     在实际应用中，通常需要采用结构数组的形式来单独保存顶点信息，然后采用二维数组的形式保存顶点之间的关系。这种保存顶点之间关系的数组称为邻接矩阵。
+        
+ */
+
+class GraphMatrix{
+    //图的最大顶点数
+    static final int MAXNUM = 20;
+    //最大值
+    static final int MAX_VALUE = 65535;
+    
+    //保存顶点信息（序号或字母）
+    char[] mVertex = new char[MAXNUM];
+    //图的类型：0：无向图； 1：有向图
+    int mGType;
+    //顶点的数量
+    int mVertexNum;
+    //边的数量
+    int mEdgeNum;
+    //保存边的权
+    int[][] mEdgeWeight = new int[MAXNUM][MAXNUM];
+    //遍历标志
+    int[] isTrav = new int[MAXNUM];
+    
+
+}
+
+
+
+public class Fig {
+    
+    /**
+     * 清空图就是将一个图结构变成一个空图，这里只需将矩阵中各元素设置为MAX_VALUE即可。
+     * @param gm：指向图结构的引用
+     */
+    static void clearGraph(GraphMatrix gm){
+        int i,j;
+        //清空矩阵
+        for (i = 0; i < gm.mEdgeNum; i++) {
+            for (j = 0; j < gm.mEdgeNum; j++) {
+                //设置矩阵中各元素的值为最大值
+                gm.mEdgeWeight[i][j] = GraphMatrix.MAX_VALUE;
+            }
+        }
+    }
+    
+    /**
+     * 显示图就是显示图的邻接矩阵，用户可以通过邻接矩阵方便地了解图的顶点和边等结构信息
+     * 在程序中，首先在第1行输出顶点信息，然后，逐个输出矩阵中的每个元素，这里，以Z表示无穷大
+     * @param gm：指向图结构的引用
+     */
+    static void outGraph(GraphMatrix gm){
+        int i,j;
+        for (j = 0; j < gm.mVertexNum; j++) {
+            //在第1行输出顶点信息
+            System.out.printf("%c",gm.mVertex);
+        }
+        for (i = 0; i < gm.mVertexNum; i++) {
+            System.out.printf("%c", gm.mVertex[i]);
+            for (j = 0; j < gm.mVertexNum; j++) {
+                //若权值为最大值
+                if (gm.mEdgeWeight[i][j] == GraphMatrix.MAX_VALUE) {
+                    //以Z表示无穷大
+                    System.out.println("Z");
+                } else {
+                    //输出边的权值
+                    System.out.printf("%d", gm.mEdgeWeight[i][j]);
+                }
+            }
+        }
+    }
+    
+    /**
+     * 创建图 在程序中，由用户输入顶点信息和边的信息。对于边来说，需要输入起始顶点、结束顶点和权值，各项以空格分割，最后，判断是否为
+     * 无向图，以为无向图还需将边的权值保存到对角位置
+     * @param gm：指向图结构的引用
+     */
+    static void createGraph(GraphMatrix gm){
+        int i,j,k;
+        //权
+        int weight;
+        //边的起始顶点
+        char eStartV, eEndV;
+        Scanner input = new Scanner(System.in);
+        
+        //输入顶点
+        System.out.println("输入图中各顶点信息");
+        for (i = 0; i < gm.mVertexNum; i++) {
+            System.out.printf("第%d个顶点：", i + 1 );
+            //保存到各顶点数组元素中
+            gm.mVertex[i] = (input.next().toCharArray()[0]);
+        }
+        
+        //输入边的信息
+        System.out.println("输入构成各边的顶点及权值：");
+        for (k = 0; k < gm.mEdgeNum; k++) {
+            System.out.printf("第%d条边： ", k + 1);
+            eStartV = input.next().charAt(0);
+            eEndV = input.next().charAt(0);
+            weight = input.nextInt();
+            //在已有顶点中查找开始点
+            for (i = 0; eStartV != gm.mVertex[i]; i++);
+            //在已有顶点中查找终点
+            for(j=0; eEndV != gm.mVertex[j]; j++);
+            //对应位置保存权值，表示有一条边
+            gm.mEdgeWeight[i][j] = weight;
+            //若是无向图
+            if (gm.mGType == 0) {
+                //在对角位置保存权值
+                gm.mEdgeWeight[i][j]=weight;
+            }
+        }
+    }
+    
+    /**
+     * 遍历图就是逐个访问图中所有的顶点。
+     * 常用的图遍历方法有两种：广度优先法和深度优先法，我们这里一深度优先遍历算法为例，深度优先类似于树的先序遍历
+     * 程序从第n个结点开始深度遍历图
+     * @param gm:指向图结构的引用
+     * @param n:顶点编号
+     */
+    static void deepTraOne(GraphMatrix gm, int n){
+        int i;
+        //标记该顶点已处理过
+        gm.isTrav[n] = 1;
+        System.out.printf("->%c", gm.mVertex[n]);
+        
+        //添加处理节点的操作
+        for (i = 0; i < gm.mVertexNum; i++) {
+            if (gm.mEdgeWeight[n][i] != GraphMatrix.MAX_VALUE && gm.isTrav[n] == 0) {
+                //递归进行遍历
+                deepTraOne(gm, i);
+            }
+        }
+        
+    }
+
+    /**
+     * 用于执行完整的深度优先遍历，以访问所有的顶点
+     * @param gm
+     */
+    static void deepTraGraph(GraphMatrix gm){
+        int i;
+        //清除各顶点遍历标志
+        for (i = 0; i < gm.mVertexNum; i++) {
+            gm.isTrav[i] = 0;
+        }
+        
+        System.out.println("深度优先遍历结点：");
+        for (i = 0; i < gm.mVertexNum; i++){
+            //若该点未遍历
+            if (gm.isTrav[i] == 0) {
+                //调用函数遍历
+                deepTraOne(gm, i);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        //定义保存邻接表结构的图
+        GraphMatrix gm = new GraphMatrix();
+        
+        System.out.println("输入生成图的类型：");
+        Scanner input = new Scanner(System.in);
+        //图的种类
+        gm.mGType = input.nextInt();
+        
+        System.out.println("输入图的顶点数量:");
+        //输入图顶点数
+        gm.mVertexNum = input.nextInt();
+        
+        System.out.println("输入图的边数量: ");
+        //输入图边的数量
+        gm.mEdgeNum = input.nextInt();
+        
+        //清空图
+        clearGraph(gm);
+        //生成邻接表结构的图
+        createGraph(gm);
+        System.out.println("该图的邻接矩阵数据如下: ");
+        //输出邻接矩阵
+        outGraph(gm);
+        //深度优先搜索遍历图
+        deepTraGraph(gm);
+    }
+
+}
--- src/com/datastruct/noline/Tree.java
+++ src/com/datastruct/noline/Tree.java
@@ -1,4 +1,7 @@
 package com.datastruct.noline;
+
+import java.util.Scanner;
+
 /**
  * 树：
  *    树结构是一种描述非线性层次关系的数据结构。树是n个数据结点的集合，在该集合中包含一个根结点，根结点之下分布着一些互不交叉的子集合
@@ -88,6 +91,297 @@ class CBTType {
     CBTType mLeft;
     //右子树结点引用
     CBTType mRight;
+    
+    /**
+     * 初始化二叉树
+     * 在使用顺序表之前，首先要初始化二叉树。程序中只需将一个结点设置为二叉树的根结点
+     * @return
+     */
+    CBTType initTree(Scanner input){
+        CBTType node;
+        //申请内存
+        if ((node = new CBTType()) != null){
+            System.out.println("请先输入一个根结点数据");
+            node.mData = input.next();
+            node.mLeft = null;
+            node.mRight = null;
+            if (node != null) {
+                return node;
+            } else {
+                return null;
+            }
+        }
+        return null;
+    }
+    
+    /**
+     * 添加结点就是在二叉树中添加结点数据，添加结点时除了要输入结点数据外，还需要指定其父结点，以及添加的结点是作为左子树还是右子树
+     * 程序中首先申请内存，然后由用户输入二叉树结点数据，并设置左右子树为空，接着指定其父结点，最后设置其作为左子树还是右子树。
+     * @param treeNode:二叉树的根结点（传入根结点便于代码中进行查找）
+     */
+    void addTreeNode(CBTType treeNode,Scanner input){
+        CBTType pnode, parent;
+        String data;
+        int menusel;
+        
+        //分配内存
+        if ((pnode = new CBTType()) != null) {
+            System.out.println("输入二叉树结点数据：");
+            pnode.mData = input.next();
+            //设置左右子树为空
+            pnode.mLeft = null;
+            pnode.mRight = null;
+            
+            System.out.println("输入该结点的父结点数据：");
+            
+            data = input.next();
+            //查找指定数据的结点
+            parent = findTreeNode(treeNode, data);
+            //如果未找到
+            if (parent == null) {
+                System.out.println("未找到该父结点！");
+                //释放创建的结点内存
+                pnode = null;
+                return;
+            }
+            System.out.println("1.添加该结点到左子树，2.添加该结点到右子树。");
+            do{
+                //输入选择项
+                menusel = input.nextInt();
+                if (menusel == 1 || menusel == 2) {
+                    if (parent == null) {
+                        System.out.println("不存在父结点，请先设置父结点！");
+                    } else {
+                        switch(menusel){
+                        case 1:
+                            //添加到左结点
+                            if (parent.mLeft != null) {
+                                System.out.println("左子树结点不为空！");
+                            } else {
+                                parent.mLeft = pnode;
+                            }
+                            break;
+                        case 2:
+                            //添加到右结点
+                            if (parent.mRight != null) {
+                                System.out.println("右子树结点不为空！");
+                            } else {
+                                parent.mRight = pnode;
+                            }
+                            break;
+                         default:
+                             System.out.println("无效参数！");
+                             break;
+                        }
+                    }
+                }
+            }while(menusel != 1 && menusel != 2);
+        }
+    }
+
+    /**
+     * 查找结点就是遍历二叉树中的每个结点中，逐个比较数据，当找到目标数据时将返回该数据所在结点的引用。程序中首先判断根结点是否为空
+     * ，然后分别向左右子树递归查找。如果当前结点的数据与查找数据相等，则返回当前结点的引用
+     * @param treeNode:待查找的二叉树的根结点
+     * @param data:待查找的结点数据
+     * @return
+     */
+    CBTType findTreeNode(CBTType treeNode, String data){
+        CBTType ptr;
+        if (treeNode == null) {
+            return null;
+        } else {
+            if (treeNode.mData.equals(data)) {
+                return treeNode;
+            } else {
+                //分别向左右子树递归查找
+                if ((ptr = findTreeNode(treeNode.mLeft, data)) != null) {
+                    return ptr;
+                } else if ((ptr = findTreeNode(treeNode.mRight, data)) != null) {
+                    return ptr;
+                } else {
+                    return null;
+                }
+            }
+        }
+    }
+    
+    /**
+     * 获取左子树就是返回当前结点的左子树结点的值
+     * 程序将返回该结点的左子树的引用
+     * @param treeNode:二叉树的一个结点
+     * @return
+     */
+    CBTType leftTreeNode(CBTType treeNode){
+        if (treeNode != null) {
+            return treeNode.mLeft;
+        }
+        return null;
+    }
+    
+    /**
+     * 获取右子树就是返回当前结点的右子树结点的值
+     * 程序将返回该结点的右子树的引用
+     * @param treeNode:二叉树的一个结点
+     * @return
+     */
+    CBTType rightTreeNode(CBTType treeNode){
+        if (treeNode != null) {
+            return treeNode.mRight;
+        }
+        return null;
+    }
+    
+    /**
+     * 计算二叉树深度就是计算二叉树中结点的最大层数。程序中首先判断根结点是否为空，然后分别按照递归调用来计算左子树和右子树的深度，
+     * 从而完成整个二叉树深度的计算
+     * @param treeNode:待计算的二叉树的根结点
+     * @return
+     */
+    int treeDepth(CBTType treeNode){
+        int depleft, depright;
+        if (treeNode == null){
+            //对于空树，深度为0
+            return 0;
+        } else {
+            //左子树深度（递归调用）
+            depleft = treeDepth(treeNode.mLeft);
+            //右子树深度（递归调用）
+            depright = treeDepth(treeNode.mRight);
+            if (depleft > depright) {
+                return depleft + 1;
+            } else {
+                return depright + 1;
+            }
+        }
+    }
+    
+    /**
+     * 清空二叉树就是将二叉树变成一个空树。这里需要使用递归算法来实现。程序中按照递归调用来清空左右子树，并且使用赋值null操作来释放
+     * 当前结点所占内存，从而完成清空操作。
+     * @param treeNode:待清空的二叉树的根结点
+     */
+    void clearTree(CBTType treeNode){
+        if (treeNode != null) {
+            //清空左子树
+            clearTree(treeNode.mLeft);
+            //清空右子树
+            clearTree(treeNode.mRight);
+            //释放当前结点所占内存
+            treeNode = null;
+        }
+    }
+    
+    /**
+     * 显示结点就是显示当前结点的数据内容
+     * @param p:待显示的结点
+     */
+    void treeNodeData(CBTType p){
+        System.out.printf("%s", p.mData);
+    }
+    
+  /**
+    遍历二叉树：逐个查找二叉树中所有的结点。
+    
+    按层遍历
+        二叉树代码的是一种层次结构，因此，我们首先想到的便是按层来遍历整个二叉树。对于二叉树的按层遍历，一般不能使用递归算法来编码，而是
+        使用一个循环队列来进行处理。首先将第1层（根结点）进入队列，再将第1根结点的左右子树（第2层）进入队列,就这样循环处理，可以逐层遍历。
+    
+    使用递归遍历
+        先序遍历：即先访问根结点，再按先序遍历左子树，最后按先序遍历右子树。先序遍历一般也称为先根次序遍历，简称DLR遍历
+        中序遍历：即先按中序遍历左子树，再访问根结点，最后按中序遍历右子树。中序遍历一般也称为中根次序遍历，简称LDR遍历
+        后序遍历：即先按后序遍历左子树，再按后序遍历右子树，最后访问根结点。后序遍历一般也称为后根次序遍历，简称LRD遍历
+        
+        先序遍历，中序遍历和后序遍历的最大好处便是可以方便地利用递归的思想来实现遍历算法
+  */
+    
+    /**
+     * 按层遍历是最直观的遍历算法。首先处理第1层（根结点），再处理第1层根结点的左右子树，即是第2层，....就这样循环处理，就可以逐层遍历。
+     * 程序在整个处理过程中，首先从根结点开始，将每层的结点逐步进入队列，这样就可得到按层遍历的效果
+     * @param treeNode:需要遍历的二叉树根结点
+     */
+    void levelTree(CBTType treeNode){
+        CBTType p;
+        //定义一个顺序栈
+        CBTType[] q = new CBTType[MAXLEN];
+        int head = 0,tail = 0;
+        
+        //如果队首引用不为空
+        if (treeNode != null) {
+            //计算循环队列队尾序号
+            tail = (tail + 1) % MAXLEN;
+            //将二叉树根引用进队
+            q[tail] = treeNode;
+        }
+        
+        //队列不为空，进行循环
+        while(head != tail){
+            //计算循环队列的队首序号
+            head = (head + 1) % MAXLEN;
+            //获取队首元素
+            p = q[head];
+            //处理队首元素
+            treeNodeData(p);
+            //如果结点存在左子树
+            if (p.mLeft != null) {
+                //计算循环队列的队尾序号
+                tail = (tail + 1) % MAXLEN;
+                //将左子树引用进队
+                q[tail] = p.mLeft;
+            }
+            //如果结点存在右子树
+            if (p.mRight != null){
+                //计算循环队列的队尾序号
+                tail = (tail + 1) % MAXLEN;
+                //将右子树引用进队
+                q[tail] = p.mRight;
+            }
+                    
+        }
+    }
+    
+    /**
+     * 先序遍历就是先按中序遍历左子树，再访问根结点，最后按中序遍历右子树。程序中可以按照递归的思路来遍历整个二叉树
+     * @param treeNode:需要遍历的二叉树根结点
+     */
+    void treeDLR(CBTType treeNode){
+        if (treeNode != null) {
+            //显示结点的数据
+            treeNodeData(treeNode);
+            treeDLR(treeNode.mLeft);
+            treeDLR(treeNode.mRight);
+        }
+    }
+    
+    /**
+     * 中序遍历就是先访问根结点，再按先序遍历左子树，最后按先序遍历右子树。程序中可以按照递归的思路来遍历整个二叉树
+     * @param treeNode:需要遍历的二叉树根结点
+     */
+    void treeLDR(CBTType treeNode){
+        if (treeNode != null) {
+            //中序遍历左子树
+            treeLDR(treeNode.mLeft);
+            //显示结点数据
+            treeNodeData(treeNode);
+            //中序遍历右子树
+            treeLDR(treeNode.mRight);
+        }
+    }
+    
+    /**
+     * 后序遍历就是先按后序遍历左子树，再按后序遍历右子树，最后访问根结点。程序中可以按照递归的思路来遍历整个二叉树
+     * @param treeNode:需要遍历的二叉树根结点
+     */
+    void treeLRD(CBTType treeNode){
+        if (treeNode != null) {
+            //后序遍历左子树
+            treeLRD(treeNode.mLeft);
+            //后序遍历右子树
+            treeLRD(treeNode.mRight);
+            //显示结点数据
+            treeNodeData(treeNode);
+        }
+    }
 }
 
 
@@ -95,6 +389,78 @@ class CBTType {
 public class Tree {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //root为指向二叉树根结点的引用
+        CBTType root = null;
+        
+        int menusel;
+        CBTType t = new CBTType();
+        //设置根元素
+        root = t.initTree(input);
+        
+        //添加结点
+        do {
+            System.out.println("请选择菜单添加二叉树的结点");
+            //显示菜单
+            System.out.println("0.退出");
+            System.out.println("1.添加二叉树的结点");
+            menusel = input.nextInt();
+            switch (menusel) {
+            case 1:
+                //添加结点
+                t.addTreeNode(root, input);
+                break;
+            case 0:
+                break;
+            default:
+                break;
+            }
+        } while(menusel != 0);
+        
+        //遍历
+        do {
+            System.out.println("请选择菜单遍历二叉树，输入0表示退出：");
+            //显示菜单
+            System.out.println("1.先序遍历DLR");
+            System.out.println("2.中序遍历LDR");
+            System.out.println("3.后序遍历LRD");
+            System.out.println("4.按层遍历");
+            menusel = input.nextInt();
+            switch (menusel){
+            case 0:
+                break;
+            case 1:
+                //先序遍历
+                System.out.println("先序遍历DLR的结果：");
+                t.treeDLR(root);
+                System.out.println("");
+                break;
+            case 2:
+                //中序遍历
+                System.out.println("中序遍历LDR的结果：");
+                t.treeLDR(root);
+                System.out.println("");
+                break;
+            case 3:
+                //后序遍历
+                System.out.println("后序遍历LRD的结果：");
+                t.treeLRD(root);
+                System.out.println("");
+                break;
+            case 4:
+                //按层遍历
+                System.out.println("按层遍历的结果：");
+                t.levelTree(root);
+                System.out.println("");
+                break;
+            }
+        } while(menusel != 0);
+        
+        //深度
+        System.out.printf("二叉树深度为：%d", t.treeDepth(root));
+        //清空二叉树
+        t.clearTree(root);
+        root = null;
     }
 
 }
